cd8952e89119969bbab59ac19e5d0d5ae3f494c9
remi <remi.girard@ntymail.com>
2020-07-02 22:06:12 +0200

    prod: add key prod links

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    youtubeAPIKey: process.env.youtubeAPIKey,
+    githubClientID: process.env.GITHUB_CLIENT_ID,
+    githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+    mongoURI: process.env.mongoURI,
+    cookieKey: process.env.cookieKey,
+    redirectDomain: process.env.redirectDomain
+};
