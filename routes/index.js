2200642e6c054eb0bdd036efd1796bf8cf9595a1
remi <remi.girard@ntymail.com>
2020-07-03 20:24:52 +0200

    prod: configure deployment

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Captain Fallacy' });
-});
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Captain Fallacy' });
+//});
 
 module.exports = router;
