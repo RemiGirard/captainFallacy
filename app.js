d3b7f133319b732b8abef737c41f1746ee365587
remi <remi.girard@ntymail.com>
2020-07-03 19:15:16 +0200

    prod: configure deployment

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/auth', authenticationRouter);
 app.use('/api', requireLogin, apiRouter);
 // app.use('/api', requireLogin, apiRouter);
